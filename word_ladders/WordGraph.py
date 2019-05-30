7798e352afb5c7428da52a5b755844c797ee78b0
Rob Alelyunas <RobAlelyunas@yahoo.com>
2019-05-30 12:41:49 -0400

    !

--- word_ladders/WordGraph.py
+++ word_ladders/WordGraph.py
@@ -11,14 +11,6 @@ class WordGraph():
     def get_related(self, word):
         return self.words[word]
 
-    def word_distance_is_one(self, word1, word2):
-        if len(word1) != len(word2):
-            return False
-        for c in range(0,len(word1)):
-            if word1[c] != word2[c]:
-                return word1[c + 1:] == word2[c + 1:]
-        return False
-
 '''
 This generates a json structure that shows for each word all the words that are one character away.  It takes about an hour
 to run currently so only needs to be done when a new word list is used
@@ -29,7 +21,7 @@ def generate_relations_file():
     with open(word_list_file_name) as word_list_file:
         content = word_list_file.readlines()
     content = [x.strip() for x in content]
-    self.words = {word:[] for word in content}
+    words = {word:[] for word in content}
     words_by_len = {}
     for word in content:
         try:
@@ -42,12 +34,18 @@ def generate_relations_file():
     for word1 in content:
         print(word1)
         counter = counter + 1
+        if (counter > 200):
+            break
         for word2 in words_by_len[len(word1)]:
-            if self.word_distance_is_one(word1, word2):
-                self.words[word1].append(word2)
+            for c in range(0,len(word1)):
+                if word1[c] != word2[c]:
+                    if word1[c + 1:] == word2[c + 1:]:
+                        words[word1].append(word2)
+                    else:
+                        break
     relation_dict_name = os.path.join(current_dir,'resources','generated_relations.json')
     with open(relation_dict_name, 'w') as relation_dict_file:
-        json.dump(self.words, relation_dict_file, indent=2)
+        json.dump(words, relation_dict_file, indent=2)
         
 if __name__ ==  "__main__":
     generate_relations_file()
